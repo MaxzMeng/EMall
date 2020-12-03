cbe3f21c34c2518e5b1f4ee475f4ef80be67fcd8
MaxzMeng <maxzmeng@gmail.com>
2020-12-03 23:49:37 +0800

    [NewFeature] add banner library
    Signed-off-by: MaxzMeng <maxzmeng@gmail.com>

---
+++ hi-ui/HiUi/src/main/java/me/maxandroid/hiui/banner/HiBanner.java
@@ -0,0 +1,99 @@
+package me.maxandroid.hiui.banner;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.List;
+
+import me.maxandroid.hiui.R;
+import me.maxandroid.hiui.banner.core.HiBannerDelegate;
+import me.maxandroid.hiui.banner.core.HiBannerMo;
+import me.maxandroid.hiui.banner.core.IBindAdapter;
+import me.maxandroid.hiui.banner.core.IHiBanner;
+import me.maxandroid.hiui.banner.indicator.HiIndicator;
+
+public class HiBanner extends FrameLayout implements IHiBanner {
+    public HiBannerDelegate delegate;
+
+    public HiBanner(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public HiBanner(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public HiBanner(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        delegate = new HiBannerDelegate(getContext(), this);
+        initCustomAttrs(context, attrs);
+    }
+
+    private void initCustomAttrs(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.HiBanner);
+        boolean autoPlay = typedArray.getBoolean(R.styleable.HiBanner_autoPlay, true);
+        boolean loop = typedArray.getBoolean(R.styleable.HiBanner_loop, false);
+        int intervalTime = typedArray.getInteger(R.styleable.HiBanner_intervalTime, -1);
+        setAutoPlay(autoPlay);
+        setLoop(loop);
+        setIntervalTime(intervalTime);
+        typedArray.recycle();
+    }
+
+    @Override
+    public void setBannerData(@LayoutRes int layoutResId, @NonNull List<? extends HiBannerMo> models) {
+        delegate.setBannerData(layoutResId, models);
+    }
+
+    @Override
+    public void setBannerData(@NonNull List<? extends HiBannerMo> models) {
+        delegate.setBannerData(models);
+    }
+
+    @Override
+    public void setHiIndicator(HiIndicator hiIndicator) {
+        delegate.setHiIndicator(hiIndicator);
+    }
+
+    @Override
+    public void setAutoPlay(boolean autoPlay) {
+        delegate.setAutoPlay(autoPlay);
+    }
+
+    @Override
+    public void setLoop(boolean loop) {
+        delegate.setLoop(loop);
+    }
+
+    @Override
+    public void setIntervalTime(int intervalTime) {
+        delegate.setIntervalTime(intervalTime);
+    }
+
+    @Override
+    public void setBindAdapter(IBindAdapter bindAdapter) {
+        delegate.setBindAdapter(bindAdapter);
+    }
+
+    @Override
+    public void setOnPageChangeListener(ViewPager.OnPageChangeListener onPageChangeListener) {
+        delegate.setOnPageChangeListener(onPageChangeListener);
+    }
+
+    @Override
+    public void setScrollDuration(int duration) {
+        delegate.setScrollDuration(duration);
+    }
+
+    @Override
+    public void setOnBannerClickListener(OnBannerClickListener onBannerClickListener) {
+        delegate.setOnBannerClickListener(onBannerClickListener);
+    }
+}
---
+++ hi-ui/HiUi/src/main/java/me/maxandroid/hiui/banner/core/IHiBanner.java
@@ -0,0 +1,36 @@
+package me.maxandroid.hiui.banner.core;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.List;
+
+import me.maxandroid.hiui.banner.indicator.HiIndicator;
+
+public interface IHiBanner {
+    void setBannerData(@LayoutRes int layoutResId, @NonNull List<? extends HiBannerMo> models);
+
+    void setBannerData(@NonNull List<? extends HiBannerMo> models);
+
+    void setHiIndicator(HiIndicator<?> hiIndicator);
+
+    void setAutoPlay(boolean autoPlay);
+
+    void setLoop(boolean loop);
+
+    void setIntervalTime(int intervalTime);
+
+    void setBindAdapter(IBindAdapter bindAdapter);
+
+    void setOnPageChangeListener(ViewPager.OnPageChangeListener onPageChangeListener);
+
+    void setOnBannerClickListener(OnBannerClickListener onBannerClickListener);
+
+    void setScrollDuration(int duration);
+
+    interface OnBannerClickListener {
+        void onBannerClick(@NonNull HiBannerAdapter.HiBannerViewHolder viewHolder, @NonNull HiBannerMo bannerMo, int position);
+    }
+
+}
