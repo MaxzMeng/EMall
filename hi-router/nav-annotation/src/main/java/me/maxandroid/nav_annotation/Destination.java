7012ff275188f0b1f6492641cb635a9f4415f92a
MaxzMeng <maxzmeng@gmail.com>
2020-12-08 23:53:58 +0800

    [NewFeature] Add navigation compiler
    Signed-off-by: MaxzMeng <maxzmeng@gmail.com>

---
+++ hi-router/nav-annotation/src/main/java/me/maxandroid/nav_annotation/Destination.java
@@ -0,0 +1,13 @@
+package me.maxandroid.nav_annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface Destination {
+    String pageUrl();
+
+    boolean asStarter() default false;
+
+
+}
